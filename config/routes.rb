f9608773524f96e70b4d829cea01869ae1e5d0b7
Theo <theo.gianella@proton.me>
2023-02-14 14:31:26 +0100

    User routes now with resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'user/:id/show', to: 'user#show', as: 'user_show'
   get 'welcome/:first_name', to: 'welcome#user'
   get '/team', to: 'static#team'
   get '/contact', to: 'static#contact'
   resources :gossips
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
