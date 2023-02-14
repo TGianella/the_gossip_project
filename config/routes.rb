cd1947151dc996f269e5b288e4df68ef53316a26
Theo <theo.gianella@proton.me>
2023-02-14 13:56:41 +0100

    Changing routes : now with resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   get 'user/:id/show', to: 'user#show', as: 'user_show'
-  get 'gossips/index'
-  get 'gossips/:id/show', to: 'gossips#show', as: 'gossips_show'
   get 'welcome/:first_name', to: 'welcome#user'
   get '/team', to: 'static#team'
   get '/contact', to: 'static#contact'
-  resources :gossips, only: %i[new create]
+  resources :gossips
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
